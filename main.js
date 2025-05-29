83740c03f5239aee6d7ac5b5d507b3f16c9dc805
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-05-29 20:37:59 -0300

    .
    .

---
+++ main.js
@@ -0,0 +1,16 @@
+AOS.init()
+ 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
